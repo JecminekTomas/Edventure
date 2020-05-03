ba51737f57a7ed4ed6bde2b1b35127ce08d11089
xjecmine <xjecmine@mendelu.cz>
2020-05-03 17:25:16 +0200

    Working RV

--- App/src/main/java/com/example/tutorme/sharedpreferences/SharedPreferencesManager.kt
+++ App/src/main/java/com/example/tutorme/sharedpreferences/SharedPreferencesManager.kt
@@ -9,7 +9,7 @@ class SharedPreferencesManager {
         private const val fileName = "sptutorme"
         private const val firstRun = "first_run"
 
-        // INFO: Vrací objekt k přístupu k SharedPreferences, paratrem je context (který umí zachytit stav)
+        /** Vrací objekt k přístupu k SharedPreferences, paratrem je context (který umí zachytit stav) */
         private fun getSharedPreferences(context: Context): SharedPreferences {
             return context.getSharedPreferences(fileName, Context.MODE_PRIVATE)
         }
@@ -18,12 +18,12 @@ class SharedPreferencesManager {
 
 
     fun saveFirstRun(context: Context) {
-        val editor = getSharedPreferences(context).edit() // INFO: editor vstoupí do SP a umožní editaci
-        editor.putBoolean(firstRun, false) // INFO: POZOR - Musím nastavit hodnotu na false. (logicky - neběží poprvé)
+        val editor = getSharedPreferences(context).edit() /** editor vstoupí do SP a umožní editaci */
+        editor.putBoolean(firstRun, false) /** POZOR - Musím nastavit hodnotu na false. (logicky - neběží poprvé) */
         editor.apply()
     }
 
-    // Info: Vrátí false v případě, že aplikace běží poprvé
+    /** Vrátí false v případě, že aplikace běží poprvé */
     fun isRunForFirstTime(context: Context): Boolean {
         val sharedPreferences = getSharedPreferences(context)
         return sharedPreferences.getBoolean(firstRun, true)
