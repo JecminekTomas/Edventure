de5248d5331133254c341dbc6cccd4bae925aa10
xjecmine <xjecmine@mendelu.cz>
2020-06-17 20:15:57 +0200

    Startup commit

--- App/src/main/java/com/example/tutorme/model/Tutor.kt
+++ App/src/main/java/com/example/tutorme/model/Tutor.kt
@@ -10,11 +10,11 @@ import androidx.room.PrimaryKey
 
 // TODO: Tutor bude propojen s USER, což bude nad-entita
 
-@Entity(tableName = "User")
+@Entity(tableName = "tutor")
 data class Tutor(
     @PrimaryKey (autoGenerate = true)
-    @ColumnInfo(name = "userId")
-    val userId: Long? = null,
+    @ColumnInfo(name = "tutorId")
+    val tutorId: Long? = null,
 
     @ColumnInfo(name = "firstName")
     val firstName: String? = null,
@@ -25,7 +25,8 @@ data class Tutor(
     @ColumnInfo(name = "city")
     val city: String? = null,
 
-    // TODO: City nebude string, ale bude to výčet českých měst a také jich bude moct být víc
+    // TODO: Bude city v DB? ... Neexistuje jiná kontrola města?
+    // TODO: City nebude string, ale bude to výčet českých měst a také jich bude moct být víc.. b
     /** Bude city entita? - Nejspíš ne. Bude potřeba vytvořit nějaké konstanty ze začátku českých měst */
 
     @ColumnInfo(name = "phoneNumber")
@@ -36,14 +37,20 @@ data class Tutor(
     val email: String? = null,
     // TODO: Předělat na typ e-mail, pokud je.
 
+    // TODO: Vymazat ... Cena může být rozdílá u kurzů
     @ColumnInfo(name = "pricePerHour")
     val pricePerHour: Double? = null,
 
     @ColumnInfo(name = "rating")
     val rating: Double? = null
 
-    //TODO: Je potřeba vytvořit stupnici od 0 do 5
-    // TODO: Předělat na mutable list .. aby bylo možno vypočíst průměr hodnocení.
 
+    /** TODO: Je potřeba vytvořit stupnici od 0 do 5
+     ** TODO: Předělat na mutable list .. aby bylo možno vypočíst průměr hodnocení. .. Nebo NE.
+
+     ** TODO: Nebude to USER, nejedná se o třídu, ale o ENTITU!
+     ** TODO: Bude jenom jednou v RV, ale pokud bude mít rozdílné sazby, stanoví se rozmezí.
+     ** TODO: Pokud bude uživatel vyhledávat dle předmětu, zobrazí se již jenom cena za předmět.
+     */
 
 )
