10f5c52981e8a4a1511c0bab80b20dc96d103dfa
xjecmine <xjecmine@mendelu.cz>
2020-07-07 13:46:55 +0200

    New: Menu of FilterTutor, Slider.

--- App/src/main/java/com/example/tutorme/activities/FilterTutorActivity.kt
+++ App/src/main/java/com/example/tutorme/activities/FilterTutorActivity.kt
@@ -1,13 +1,14 @@
 package com.example.tutorme.activities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import com.example.arch.BaseActivity
 import com.example.tutorme.R
+import kotlinx.android.synthetic.main.activity_select_tutor.toolbar
 
 class FilterTutorActivity: BaseActivity() {
-
-
 
     companion object {
         fun createIntent(context: Context): Intent {
@@ -16,4 +17,15 @@ class FilterTutorActivity: BaseActivity() {
     }
 
     override val layout: Int = R.layout.activity_filter_tutor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+    }
 }
